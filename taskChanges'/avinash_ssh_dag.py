13dab44bd2cb86d2aafd8ef8425b4f7f23506a31
avinashCTR <avinash.reddy@couture.ai>
2025-02-24 15:58:49 +0530

    Update avinash_ssh_dag.py

--- taskChanges'/avinash_ssh_dag.py
+++ taskChanges'/avinash_ssh_dag.py
@@ -16,22 +16,22 @@ default_args = {
 env_command = "conda activate /data/archita/searchengine_nlp"
 bash_command = textwrap.dedent(
         f"""
-        {env_command} && /app/notebooks/avinash/ipa-task.sh \
+        {env_command} && sh /app/notebooks/avinash/IPA-TASK/ipa-task.sh \
         --w2r-path "/data1/searchengine/processed/jiomart/05092024/V6_delta_changes/Unified3VerticalsSelectAttributes/W2RAllVariantsCleaned" \
         --ipa-meta-path "/data1/searchengine/processed/jiomart/accumulateddata/IPATransliterationsAutomaticAccumulator20250117" \
-        --w2r-scored-path "/data1/archive/avinash/W2RWithIPATransliterations.parquet" \
-        --ipa-meta-output-path "/data1/archive/avinash/IPATransliterationsAutomaticAccumulator_avinash.parquet" \
+        --w2r-scored-path "/data1/archive/avinash/W2RWithIPATransliterations" \
+        --ipa-meta-output-path "/data1/archive/avinash/IPATransliterationsAutomaticAccumulator_avinash" \
         --cache-path "/data1/archive/avinash/CACHE" \
         --max-ipa-computations 10000
         """
 )
 
-dag = DAG('avinash_ssh_dag', default_args=default_args, schedule_interval=timedelta(days=1))
+dag = DAG('avinash_ssh_dag', default_args=default_args,schedule_interval=None)
 
 
 t1 = SSHOperator(
     task_id="ssh_task",
-    ssh_conn_id="ssh_default",
-    command=f"""ssh couture@JMNGD1BAE210V10 "{bash_command}" """,
+    ssh_conn_id="AIRFLOW_CONN_SSH_SERVER",
+    command=f"""ssh -p 8509 jioapp@10.166.181.219 "{bash_command}" """,
     dag=dag
 )
